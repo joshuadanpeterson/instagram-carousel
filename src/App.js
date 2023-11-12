ab1ca0f771c91f12a24e685439d9319b22fa39d5
Josh Peterson <joshuadanpeterson@gmail.com>
2023-11-11 18:55:17 -0700

    Added manifest.json

--- src/App.js
+++ src/App.js
@@ -1,3 +1,6 @@
+// App.js is the main component of the application. It is responsible for rendering the InstagramFeed component, and displaying the navigation buttons. It also handles the logic for the navigation buttons, and passes the appropriate props to the InstagramFeed component.
+
+// App.js Imports
 import React, { useState, useRef } from 'react';
 import InstagramFeed from './components/js/InstagramFeed';
 import Footer from './components/js/Footer';
@@ -7,6 +10,27 @@ import NavButtons from './components/js/NavButtons';
 import "./App.css";
 import "./components/css/NavigationButtons.css"
 
+// Firebase SDK
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+// App.js 
 const App = () => {
   const accessToken = process.env.REACT_APP_INSTAGRAM_ACCESS_TOKEN;
 
